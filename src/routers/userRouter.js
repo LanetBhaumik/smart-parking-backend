30d6172c9a09e4fc38ef32ac9e6b4e705c072431
LanetBhaumik <bhaumikdhameliya.lanet@outlook.com>
2022-03-28 19:18:06 +0530

    get profile route in userRouter

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -13,6 +13,27 @@ const {
   getAvatar,
 } = require("../controllers/userController");
 
+//get profile
+router.get("/profile", auth, (req, res) => {
+  try {
+    if (req.user) {
+      res.send({
+        user: req.user,
+        token: req.token,
+      });
+    } else if (req.owner) {
+      res.send({
+        owner: req.owner,
+        token: req.token,
+      });
+    }
+  } catch (error) {
+    res.status(400).send({
+      error: "bad request",
+    });
+  }
+});
+
 //new user creation
 router.post("/users", createUser);
 
