4d27aa70fbaaab1df785c5df022d09b60194c0a4
LanetBhaumik <bhaumikdhameliya.lanet@outlook.com>
2022-03-29 17:28:20 +0530

    app.js edited

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 require("./db/mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compress = require("compression");
+const methodOverride = require("method-override");
 
 const userRouter = require("./routers/userRouter");
 const carRouter = require("./routers/carRouter");
@@ -11,17 +15,37 @@ const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to smart parking");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(methodOverride());
 
 app.use(cors());
 app.use(express.json());
+
 app.use(userRouter);
 app.use(carRouter);
 app.use(ownerRouter);
 app.use(parkingRouter);
 app.use(bookingRouter);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to smart parking");
+});
 
 app.get("*", (req, res) => {
   res.send("route does not exists");
